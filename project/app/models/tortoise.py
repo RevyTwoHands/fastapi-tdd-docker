2b9807ae058a08efed3da90b98ad940db8aa4476
Revy <doretquentin@gmail.com>
2022-05-03 18:52:53 +0200

    Part 1 done

--- project/app/models/tortoise.py
+++ project/app/models/tortoise.py
@@ -1,12 +1,17 @@
 # porject/app/models/tortoise.py
 
+from typing import Text
 from tortoise import fields, models
+from tortoise.contrib.pydantic import pydantic_model_creator
 
 
 class TextSummary(models.Model):
     url = fields.TextField()
     summary = fields.TextField()
-    created_at = fields.DatetimeField(auto_nom_add=True)
+    created_at = fields.DatetimeField(auto_now_add=True)
 
     def __str__(self):
         return self.url
+
+
+SummarySchema = pydantic_model_creator(TextSummary)
